a0b8abcd7f7f41d1c080cc083d4639526e5903d6
Fonseca <gdfonseca@leroymerlin.com.br>
2021-07-09 11:33:43 -0300

    feat(goLang): exercise 1019 uri online

--- 1_beginner/1019/time_conversion.go
+++ 1_beginner/1019/time_conversion.go
@@ -8,9 +8,34 @@ type Time struct {
 	hour, minute, second int
 }
 
-func convert(input int) Time {
-	// 1 min has 60 seconds
-	// 1 hour has 3600 seconds
+/* I'm not using math.Round because it was introduced
+ * on version 1.10 and, the code must run on version 1.8
+ */
+func round(unit float64) float64 {
+	intPart := int(unit)
+	decimalPart := unit - float64(intPart)
+
+	if decimalPart >= 0.5 {
+		return float64(intPart) + 1
+	}
+
+	return float64(intPart)
+}
+
+func convert(reference int, input int) int {
+	unit := float64(input) / float64(reference)
+	intPart := int(unit)
+	unit -= float64(intPart)
+	unit *= 60
+
+	if reference == 60 {
+		return int(round(unit))
+	}
+
+	return int(unit)
+}
+
+func calculate(input int) Time {
 	var time Time
 
 	if input < 60 {
@@ -23,35 +48,15 @@ func convert(input int) Time {
 
 	if input >= 60 && input < 3600 {
 		time.hour = 0
-
 		time.minute = input / 60
-
-		seconds := float64(input) / 60.0
-		intPart := int(seconds)
-		seconds -= float64(intPart)
-
-		seconds *= 60
-
-		time.second = int(seconds)
+		time.second = convert(60, input)
 
 		return time
 	}
 
 	time.hour = input / 3600
-
-	minutes := float64(input) / 3600.0
-	intPart := int(minutes)
-	minutes -= float64(intPart)
-
-	time.minute = int(minutes)
-
-	seconds := float64(input) / 60.0
-	intPart = int(seconds)
-	seconds -= float64(intPart)
-
-	seconds *= 60
-
-	time.second = int(seconds)
+	time.minute = convert(3600, input)
+	time.second = convert(60, input)
 
 	return time
 }
@@ -61,7 +66,7 @@ func main() {
 
 	fmt.Scanf("%d", &input)
 
-	result := convert(input)
+	result := calculate(input)
 
 	fmt.Printf("%d:%d:%d\n", result.hour, result.minute, result.second)
 }
