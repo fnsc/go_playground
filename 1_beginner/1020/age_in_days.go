d890149910ab7844a2eec7c2091aa71ac4beb61f
Fonseca <gdfonseca@leroymerlin.com.br>
2021-07-09 19:31:18 -0300

    feat(goLang): exercise 1020 uri online

--- 1_beginner/1020/age_in_days.go
+++ 1_beginner/1020/age_in_days.go
@@ -8,75 +8,13 @@ type Result struct {
 	year, month, day int
 }
 
-/*
- * I'm not using math.Round because it was introduced
- * on version 1.10 and, the code must run on version 1.8
- */
-func round(unit float64) float64 {
-	intPart := int(unit)
-	decimalPart := unit - float64(intPart)
-
-	if decimalPart >= 0.1 {
-		return float64(intPart) + 1
-	}
-
-	return float64(intPart)
-}
-
-func convert(reference float64, factor float64, input int) int {
-	unit := float64(input) / float64(reference)
-	intPart := int(unit)
-	unit -= float64(intPart)
-	unit *= float64(factor)
-
-	if reference == factor {
-		return int(round(unit))
-	}
-
-	return int(unit)
-}
-
 func calculate(days int) Result {
 	var result Result
-	const MONTH = 30.416666667
-	const YEAR = 12.166666667
-
-	if days < 30 {
-		result.year = 0
-		result.month = 0
-		result.day = days
-
-		return result
-	}
-
-	if days == 30 {
-		result.year = 0
-		result.month = 1
-		result.day = 0
-
-		return result
-	}
-
-	if days == 365 {
-		result.year = 1
-		result.month = 0
-		result.day = 0
-
-		return result
-	}
-
-	if days >= 30 && days < 365 {
-
-		result.year = 0
-		result.month = int(float64(days) / MONTH)
-		result.day = convert(MONTH, MONTH, days)
-
-		return result
-	}
-
 	result.year = days / 365
-	result.month = convert(365, YEAR, days)
-	result.day = convert(MONTH, MONTH, days)
+	days %= 365
+	result.month = days / 30
+	days %= 30
+	result.day = days
 
 	return result
 }
