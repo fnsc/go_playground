fc05bd196bde9099d58f5c74376a8da01b84f881
Gabriel Fonseca <gabrieldfnsc@gmail.com>
2022-01-20 22:39:54 -0300

    exe(1151): solution + doc

---
+++ 1_beginner/1100/1151/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	input := 0
+
+	read(&input)
+
+	fibonacci := getFibonacci(&input)
+
+	printFibonacci(&fibonacci)
+}
+
+func read(input *int) {
+	fmt.Scanf("%d", input)
+
+	if *input <= 0 && *input >= 46 {
+		read(input)
+	}
+}
+
+func getFibonacci(target *int) []int {
+	result := []int{0, 1}
+	nextValue := 0
+
+	for len(result) < *target {
+		nextValue = result[len(result)-2] + result[len(result)-1]
+
+		result = append(result, nextValue)
+	}
+
+	return result
+}
+
+func printFibonacci(fibonacci *[]int) {
+	result := ""
+
+	for _, number := range *fibonacci {
+		result += fmt.Sprintf("%d ", number)
+	}
+
+	result = strings.TrimRight(result, " ")
+
+	fmt.Println(result)
+}
